80aa455dc0e1d3c223262e66f21565763e6f9e6e
Milosz <m.branewicz@wp.pl>
2021-09-26 15:41:45 +0200

    Add mobile navbar functionality

--- src/components/ActiveLink/ActiveLink.js
+++ src/components/ActiveLink/ActiveLink.js
@@ -1,12 +1,21 @@
 import { useRouteMatch } from "react-router";
 import { Link } from "react-router-dom";
 
-function ActiveLink({  to, activeOnlyWhenExact, label }) {
+function ActiveLink({ to, activeOnlyWhenExact, label, className, onClick }) {
   let match = useRouteMatch({
     path: to,
     exact: activeOnlyWhenExact,
   });
 
-  return <Link to={to} style={match ? { color: '#F24E1E' }: null}>{label}</Link>;
+  return (
+    <Link
+      className={className}
+      to={to}
+      style={match ? { color: "#F24E1E" } : null}
+      onClick={onClick}
+    >
+      {label}
+    </Link>
+  );
 }
 export default ActiveLink;
--- src/components/SiteHeader/SiteHeader.js
+++ src/components/SiteHeader/SiteHeader.js
@@ -1,35 +1,71 @@
 import ActiveLink from "../ActiveLink/ActiveLink";
 import styles from "./siteHeader.module.css";
+import { BsFillHeartFill } from "react-icons/bs";
+import { FcMenu } from "react-icons/fc";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 export default function SiteHeader() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header>
-      <div className={styles.headerImage}/>
+      <div className={styles.headerImage} />
       <nav>
         <div className={styles.headerLinks}>
-          <div className={styles.headerItems}>
-            <ActiveLink to="/" activeOnlyWhenExact={true} label="Home" />
+          <Link to="/">
+            <BsFillHeartFill className="inline text-2xl cursor-pointer" />
+          </Link>
+
+          <div className={isOpen ? styles.headerItemsOpen : styles.headerItems}>
+            <ActiveLink
+              to="/"
+              activeOnlyWhenExact={true}
+              label="Home"
+              className={isOpen ? styles.navItemOpen : styles.navItem}
+              onClick={() => setIsOpen(false)}
+            />
             <ActiveLink
               to="/heroes"
               activeOnlyWhenExact={false}
               label="Heroes"
+              className={isOpen ? styles.navItemOpen : styles.navItem}
+              onClick={() => setIsOpen(false)}
             />
-            <ActiveLink to="/items" activeOnlyWhenExact={true} label="Items" />
+            <ActiveLink
+              to="/items"
+              activeOnlyWhenExact={true}
+              label="Items"
+              className={isOpen ? styles.navItemOpen : styles.navItem}
+              onClick={() => setIsOpen(false)}
+            />
             <ActiveLink
               to="/buildings"
               activeOnlyWhenExact={true}
               label="Buildings"
+              className={isOpen ? styles.navItemOpen : styles.navItem}
+              onClick={() => setIsOpen(false)}
             />
             <ActiveLink
               to="/activities"
               activeOnlyWhenExact={true}
               label="Activities"
+              className={isOpen ? styles.navItemOpen : styles.navItem}
+              onClick={() => setIsOpen(false)}
             />
             <ActiveLink
               to="/beginner-tips"
               activeOnlyWhenExact={true}
               label="Beginner Tips"
+              className={isOpen ? styles.navItemOpen : styles.navItem}
+              onClick={() => setIsOpen(false)}
             />
           </div>
+          <FcMenu
+            onClick={() => {
+              setIsOpen(!isOpen);
+            }}
+            className={styles.menuIcon}
+          />
         </div>
       </nav>
     </header>
