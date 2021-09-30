058fdb946f6e9a9bf280386988bf7e150f834202
Milosz <m.branewicz@wp.pl>
2021-09-30 19:39:06 +0200

    Fetch hero by id on hero page, display hero image and name

--- src/pages/Hero/Hero.js
+++ src/pages/Hero/Hero.js
@@ -1,11 +1,35 @@
 import React from "react";
 import { useParams } from "react-router";
+import { gql, useQuery } from "@apollo/client";
+const HERO = gql`
+  query hero($id: ID!) {
+    hero(id: $id) {
+      id
+      hero_name
+      hero_image {
+        url
+        name
+      }
+      faction {
+        faction_name
+        faction_image {
+          url
+          name
+        }
+      }
+    }
+  }
+`;
 export default function Hero() {
   const { id } = useParams();
-  console.log(id);
-  return (
-    <div>
-      <h1>Simple hero page {id}</h1>
-    </div>
-  );
+  const { loading, error, data } = useQuery(HERO, { variables: { id } });
+  
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error...</p>;
+  const hero = {...data.hero}
+  console.log(hero);
+  return <section>
+    <h1>{hero.hero_name}</h1>
+    <img src={hero.hero_image.url} width={150} height={150} alt={hero.hero_image.name}/>
+  </section>;
 }
